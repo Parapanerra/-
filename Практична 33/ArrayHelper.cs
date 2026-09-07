703e5c7d7f38efa4673f4e233bc870739c447368
Parapanerra <tom48102@gmail.com>
2026-09-07 17:15:03 +0300

    Add practical assignment 33

---
+++ Практична 33/ArrayHelper.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+public static class ArrayHelper
+{
+    public static int IndexOf<T>(T[] array, T value)
+    {
+        for (int i = 0; i < array.Length; i++)
+        {
+            if (object.Equals(array[i], value))
+            {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
+    public static void Reverse<T>(T[] array)
+    {
+        int left = 0;
+        int right = array.Length - 1;
+
+        while (left < right)
+        {
+            T temp = array[left];
+            array[left] = array[right];
+            array[right] = temp;
+
+            left++;
+            right--;
+        }
+    }
+
+    public static T Min<T>(T[] array) where T : IComparable<T>
+    {
+        if (array.Length == 0)
+        {
+            throw new ArgumentException("Масив не може бути порожнім.");
+        }
+
+        T min = array[0];
+
+        for (int i = 1; i < array.Length; i++)
+        {
+            if (array[i].CompareTo(min) < 0)
+            {
+                min = array[i];
+            }
+        }
+
+        return min;
+    }
+
+    public static T Max<T>(T[] array) where T : IComparable<T>
+    {
+        if (array.Length == 0)
+        {
+            throw new ArgumentException("Масив не може бути порожнім.");
+        }
+
+        T max = array[0];
+
+        for (int i = 1; i < array.Length; i++)
+        {
+            if (array[i].CompareTo(max) > 0)
+            {
+                max = array[i];
+            }
+        }
+
+        return max;
+    }
+}
---
+++ Практична 33/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] numbers = { 8, 3, 15, 1, 10, 3 };
+
+        Console.WriteLine("Масив чисел:");
+        PrintArray(numbers);
+
+        Console.WriteLine("iндекс знайденого елемента: " + ArrayHelper.IndexOf(numbers, 15));
+        Console.WriteLine("мiнiмальне: " + ArrayHelper.Min(numbers));
+        Console.WriteLine("максимальне: " + ArrayHelper.Max(numbers));
+
+        ArrayHelper.Reverse(numbers);
+
+        Console.WriteLine("Пiсля розвороту масиву:");
+        PrintArray(numbers);
+
+        string[] words = { "яблуко", "банан", "груша", "апельсин" };
+
+        Console.WriteLine();
+        Console.WriteLine("Масив рядкiв:");
+        PrintArray(words);
+
+        Console.WriteLine("iндекс знайденого елемента (\"груша\"): " + ArrayHelper.IndexOf(words, "груша"));
+        Console.WriteLine("мiнiмальне: " + ArrayHelper.Min(words));
+        Console.WriteLine("максимальне : " + ArrayHelper.Max(words));
+
+        ArrayHelper.Reverse(words);
+
+        Console.WriteLine("Пiсля розвороту масиву:");
+        PrintArray(words);
+    }
+
+    static void PrintArray<T>(T[] array)
+    {
+        for (int i = 0; i < array.Length; i++)
+        {
+            Console.Write(array[i]);
+
+            if (i < array.Length - 1)
+            {
+                Console.Write(", ");
+            }
+        }
+
+        Console.WriteLine();
+    }
+}
