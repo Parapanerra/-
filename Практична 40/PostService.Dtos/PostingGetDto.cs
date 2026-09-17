9bb9f14202cb476a1d85ccf70de87c0a681f25aa
Tom <tom@example.com>
2026-09-17 11:32:36 +0300

    одано практичну 40 до збірки практичних

---
+++ Практична 40/PostService.Dtos/PostingGetDto.cs
@@ -0,0 +1,19 @@
+using PostService.CommonTypes;
+
+namespace PostService.Dtos;
+
+public class PostingGetDto
+{
+    public int Id { get; set; }
+    public string From { get; set; } = string.Empty;
+    public string To { get; set; } = string.Empty;
+    public string Content { get; set; } = string.Empty;
+    public DeliveryType DeliveryType { get; set; }
+    public float Weight { get; set; }
+    public int Width { get; set; }
+    public int Height { get; set; }
+    public int Depth { get; set; }
+    public float Value { get; set; }
+    public float Price { get; set; }
+    public DateTime CreatedAt { get; set; }
+}
