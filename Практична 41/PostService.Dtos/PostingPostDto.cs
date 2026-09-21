3d9f6eb76b9daa6839204ac81ba75583e5da1145
Parapanerra <tom48102@gmail.com>
2026-09-21 18:04:14 +0300

    Add practical assignment 41

---
+++ Практична 41/PostService.Dtos/PostingPostDto.cs
@@ -0,0 +1,17 @@
+using PostService.CommonTypes;
+
+namespace PostService.Dtos;
+
+public class PostingPostDto
+{
+    public string From { get; set; } = string.Empty;
+    public string To { get; set; } = string.Empty;
+    public string Content { get; set; } = string.Empty;
+    public DeliveryType DeliveryType { get; set; }
+    public float Weight { get; set; }
+    public int Width { get; set; }
+    public int Height { get; set; }
+    public int Depth { get; set; }
+    public float? Value { get; set; }
+    public string? Description { get; set; }
+}
